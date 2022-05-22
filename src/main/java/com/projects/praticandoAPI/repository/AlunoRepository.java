cc8b82e1c618c70c8ef64a2aee4baa6cbd11ba20
jggalmeida <j.g.almeida@hotmail.com>
2022-05-22 17:57:11 -0300

    Adicionando arquivos

--- src/main/java/com/projects/praticandoAPI/repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projects.praticandoAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projects.praticandoAPI.modelo.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    
-}
